7a5ae32112c09dc96746d8576e9a248741a40d68
zpawel <zpawel92@gmail.com>
2015-11-03 21:01:53 +0100

    testy

--- test/unit/exercise1.spec.js
+++ test/unit/exercise1.spec.js
@@ -10,11 +10,23 @@ describe('exercise1', function ()
         {
             expect(exercise1.getDescendingNumbers(15, 1)).toEqual('15 14 13 12 11 10 9 8 7 6 5 4 3 2 1');
         });
+        it('should return false, if numberTo is heiger the numberTo', function()
+        {
+            expect(exercise1.getDescendingNumbers(1,15)).toEqual(false);
+        });
+        it('sh0uld return false, if numberTo is string', function()
+        {
+            expect(exercise1.getDescendingNumbers(15,aaaa)).toEqual(false);
+        });
+        it('sh0uld return false, if numberFrom is string', function()
+        {
+            expect(exercise1.getDescendingNumbers(15,aaaa)).toEqual(false);
+        });
     });
 
     describe('deleteString', function ()
     {
-
+        
     });
 
     describe('stringCounter', function ()
